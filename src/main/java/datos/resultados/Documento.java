203dfb7d3b41ed55909c34d937ba8dc1fb0a584f
Enric Gil Gallen <Dcx23hj87>
2021-03-03 13:26:39 +0100

    Creacion de algunas clases 2

--- src/main/java/datos/resultados/Documento.java
+++ src/main/java/datos/resultados/Documento.java
@@ -7,5 +7,10 @@ public class Documento extends Resultado {
     int num_paguinas;
     int num_modulos;
 
-
+    public Documento(int id, double horas_totola, boolean tipo, Enum tipo1, int num_paguinas, int num_modulos) {
+        super(id, horas_totola, tipo);
+        this.tipo = tipo1;
+        this.num_paguinas = num_paguinas;
+        this.num_modulos = num_modulos;
+    }
 }
