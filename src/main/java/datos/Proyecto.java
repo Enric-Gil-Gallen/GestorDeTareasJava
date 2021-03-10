15ac11634891b6dbba7ee22058c8231580b75159
ernest <al377101@uji.es>
2021-03-10 12:56:33 +0100

    Clase Proyecto creada

---
+++ src/main/java/datos/Proyecto.java
@@ -0,0 +1,29 @@
+package datos;
+
+import java.util.LinkedList;
+
+public class Proyecto {
+    String titulo;
+    String descripcion;
+    LinkedList<Persona> Personas;
+    Persona responsable_Tarea;
+    int prioridad;
+    String fecha_Creacion;
+    String fecha_Finalizacion;
+    Boolean tarea_Finalizada;
+    Resultado resultado_Esperado;
+    LinkedList etiquetas;
+
+    public Proyecto(String titulo, String descripcion, LinkedList<Persona> personas, Persona responsable_Tarea, int prioridad, String fecha_Creacion, String fecha_Finalizacion, Boolean tarea_Finalizada, Resultado resultado_Esperado, LinkedList etiquetas) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        Personas = personas;
+        this.responsable_Tarea = responsable_Tarea;
+        this.prioridad = prioridad;
+        this.fecha_Creacion = fecha_Creacion;
+        this.fecha_Finalizacion = fecha_Finalizacion;
+        this.tarea_Finalizada = tarea_Finalizada;
+        this.resultado_Esperado = resultado_Esperado;
+        this.etiquetas = etiquetas;
+    }
+}
